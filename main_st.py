0416b102cc479ad5ae9c227b3773bd26621d0ad1
Melissa Peiffer <mbp83@nau.edu>
2020-10-06 13:41:49 -0700

    Fix typos in show_light_theme()

--- main_st.py
+++ main_st.py
@@ -39,9 +39,9 @@ def show_dark_theme():
 def show_light_theme():
     stt.set_theme({'primary': '#f63366'})
     stt.set_theme({'secondary': '#f0f2f6'})
-    stt_set_theme({'black': '#262730'})
-    stt_set_theme({'yellow': '#fffd80'})
-    stt_set_theme({'white': '#ffffff'})
+    stt.set_theme({'black': '#262730'})
+    stt.set_theme({'yellow': '#fffd80'})
+    stt.set_theme({'white': '#ffffff'})
 
 def main():
     show_menu()
